cb0725f77e156461134c3cd5bd899c28c984c267
Adhish-Krishna <adhishthesak@gmail.com>
2024-11-06 19:59:23 +0530

    Added a get route ("/")

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ const Event = require('./routes/RegisterRoutes')
 
 app.use('/event',Event);
 
+//route to ping the server
+app.get("/",(req,res)=>{
+    res.send("Server is Running!!!!");
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
